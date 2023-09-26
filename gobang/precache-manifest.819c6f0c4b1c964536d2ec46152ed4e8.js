b6966fe7469427387a208fac60cca1e1a53f79b7
diwayou <diwayou@qq.com>
2023-09-26 11:06:57 +0800

    gobang

---
+++ gobang/precache-manifest.819c6f0c4b1c964536d2ec46152ed4e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a58be424df6ab642c9dc",
+    "url": "/ai.bundle.js"
+  },
+  {
+    "revision": "25c00671734780abda44",
+    "url": "/css/app.ebbc1a29.css"
+  },
+  {
+    "revision": "25c00671734780abda44",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "5442f90f0a7d84ccc30d008200e3aee6",
+    "url": "/img/board.5442f90f.jpg"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.ffff4377.gif"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.9ad347b5.gif"
+  },
+  {
+    "revision": "5ba37b2bf69c51843c75e3e85f718a84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad347b59b92ffb056ef878804944200",
+    "url": "/img/haha.gif"
+  },
+  {
+    "revision": "ffff4377511f62df974614b099ad3f67",
+    "url": "/img/sad.gif"
+  }
+];
